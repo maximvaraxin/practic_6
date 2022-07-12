afd47f5f4dc701a41439d5e7ea7885bbbf7935d0
Максим Вараксин <maks-02@yandex.ru>
2022-07-12 16:34:50 +0300

    add app_3

--- app_3/Program.cs
+++ app_3/Program.cs
@@ -10,9 +10,65 @@ namespace App_3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main( string[] args )
         {
+            Console.Write($"Введите число для преобразования: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Число в десятичной системе счисления: { num }");
+
+            List<int> list = ConversionType( num, false );
             
+            ReverseList( ref list );
+            PrintList( list );
+        }
+
+        // переводит десятичной в двоичную систему счисления(необходим разворот результирующего списка)
+        static List<int> ConversionType(int number, bool enableLogging = false)
+        {  
+            List<int> resultList = new List<int>();
+           
+            while (number >= 1)
+            {
+                int item = number % 2;
+                resultList.Add(item);
+                number = number / 2;
+            }
+
+            if (enableLogging)
+	        {
+                foreach (var item in resultList)
+	            {
+                    Console.Write(item);
+	            }
+                Console.WriteLine();
+	        }
+                
+
+           return resultList;
+        }
+
+        // разворачивает полученный список
+        static void ReverseList( ref List<int> list)
+        {   
+            List<int> resultList = new List<int>();
+
+            for (int i = 1; i <= list.Count; i++)
+			{
+                resultList.Add( list[ list.Count - i] );
+			}
+
+            list = resultList;
+        }
+
+        // выводит список в консоль
+        static void PrintList(List<int> list)
+        {   
+            Console.Write($"Число в двоичной СС: ");
+
+            foreach (var item in list)
+	        {
+                Console.Write(item);
+	        }
         }
     }
 }
