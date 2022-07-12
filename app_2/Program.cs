617bb5dded6e578a1df38ea210bc0e7dc0618769
Максим Вараксин <maks-02@yandex.ru>
2022-07-12 18:32:05 +0300

    add app_2

--- app_2/Program.cs
+++ app_2/Program.cs
@@ -10,7 +10,40 @@ namespace App_2
     {
         static void Main(string[] args)
         {
+            int[] length = new int[3];
+           
+            for (int i = 0; i < 3; i++)
+			{
+                Console.Write($"Введите длину { i + 1 } стороны: ");
+                length[i] = Convert.ToInt32(Console.ReadLine());
+			}
+            Console.WriteLine($"------------");
+            Console.WriteLine( IsExistЕriangle(length) ? "Треугольник существует" : "Треугольник НЕ существует" );
+        }
+        
+        // проверяет может ли существовать треугольник с сторонами такой длины
+        static bool IsExistЕriangle( params int[] length)
+        {
+            bool existЕriangle = false;
+
+            if ( length[0] + length[1] > length[2] && length[0] > 0 && length[1] > 0 && length[2] > 0 )
+	        {
+                existЕriangle = true;
+	        }
+            else if ( length[0] + length[2] > length[1] && length[0] > 0 && length[1] > 0 && length[2] > 0  )
+            {
+                existЕriangle = true;
+            }
+            else if ( length[1] + length[2] > length[0] && length[0] > 0 && length[1] > 0 && length[2] > 0  )
+            {
+                existЕriangle = true;
+            }
+            else
+            {
+                existЕriangle = false;
+            }
             
+            return existЕriangle;
         }
     }
 }
