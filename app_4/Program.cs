659ce26df768178c9d9537aeda17eec24f103725
Максим Вараксин <maks-02@yandex.ru>
2022-07-12 17:07:04 +0300

    add app_4

--- app_4/Program.cs
+++ app_4/Program.cs
@@ -11,8 +11,46 @@ namespace App_4
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            Console.Write($"Введите число: ");
+            int length = Convert.ToInt32(Console.ReadLine());
             
+            PrintArray(FibonacciNumbers( length ),length);
+        }
+
+        // вычисляет первых N чисел Фибоначчи
+        static double[] FibonacciNumbers( int numbers )
+        {   
+            double[] result = new double[numbers];
+
+            for (int i = 0; i < result.Length; i++)
+			{    
+                 if ( i == 0 )
+	             {
+                    result[i] = 0;
+	             }
+                 else if ( i == 1 || i == 2)
+                 {
+                    result[i] = 1;
+                 }
+                 else
+                 {
+                    result[i] =  result[ i - 2 ] + result[ i - 1 ];
+                 }
+			}
+            
+            return result;
+        }
+
+        // выводит результат в консоль
+        static void PrintArray( double[] array, int len )
+        {   
+            Console.Write($"Первые { len } чисел Фибоначчи: ");
+
+            for (int i = 0; i < array.Length; i++)
+			{
+                Console.Write($"{array[i]} ");
+			}
         }
     }
 }
